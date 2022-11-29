8b5cd8a604ac84a90ae30a63cb3e1df95b09922e
Andrew <114303256+AnMoor@users.noreply.github.com>
2022-11-29 10:27:37 +0200

    Delete Byte-diplomwork-2 directory

--- Byte-diplomwork-2/script/script.js
+++
@@ -1,4 +0,0 @@
-$('.question-item >div').on('click', function(){
-    $(this).children('.question-item-icon').toggleClass('question-icon-rotate');
-    $(this).next('.question-item-text').slideToggle(500);
-})
